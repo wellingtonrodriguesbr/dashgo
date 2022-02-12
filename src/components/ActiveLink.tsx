5bbf727137688304daf8e0f0172c658d15c072d1
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-02-12 13:22:03 -0300

    feat: create active link

---
+++ src/components/ActiveLink.tsx
@@ -0,0 +1,37 @@
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+import { cloneElement, ReactElement } from "react";
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement;
+  shouldMatchExactHref?: boolean;
+}
+
+export function ActiveLink({
+  children,
+  shouldMatchExactHref = false,
+  ...props
+}: ActiveLinkProps) {
+  let isActive = false;
+  const { asPath } = useRouter();
+
+  if ((shouldMatchExactHref && asPath === props.href) || asPath === props.as) {
+    isActive = true;
+  }
+
+  if (
+    !shouldMatchExactHref &&
+    (asPath.startsWith(String(props.href)) ||
+      asPath.startsWith(String(props.as)))
+  ) {
+    isActive = true;
+  }
+
+  return (
+    <Link {...props}>
+      {cloneElement(children, {
+        color: isActive ? "pink.400" : "gray.50",
+      })}
+    </Link>
+  );
+}
