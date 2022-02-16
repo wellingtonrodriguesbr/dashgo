1f2623f26edc4086b47420051f72190e86e648f8
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-02-16 07:03:53 -0300

    chore: fictitious users with in faker-js library

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -20,12 +20,19 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useEffect } from "react";
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
+  useEffect(() => {
+    fetch("http:localhost:3000/api/users")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
 
   return (
     <>
